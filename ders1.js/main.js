812a1b3047d991c6927fbaaace808c277508554d
unknown <solhsimova@gmail.com>
2024-10-30 10:22:50 +0400

    task1js

---
+++ ders1.js/main.js
@@ -0,0 +1,111 @@
+// let emekhaqqi = +prompt("emek haqqini daxil edin");
+// let mebleg = +prompt("meblegi daxil edin");
+// let muddet = +prompt("muddeti daxil edin");
+// let ayliqodenis = +prompt("ayliq odenisi daxil edin")
+
+// if (muddet > 36){
+//     alert("size bu sertlerde  kredit dusmur");
+//     console.error("maksimal muddet 36 aydir")
+// }else if(mebleg > emekhaqqi * 10){
+//     alert("size bu sertlerde kredit dusmur");
+//     console.log("bu meblegde kredit dusmur")
+// }else if( ayliqodenis > emekhaqqi * 0.45){
+//     alert ("size bu sertlerde kredit dusmur");
+//     console.log("ayliq odenis emekhaqqinin 45% den cox olmamalidir");
+
+// }else{
+//     if(muddet <= 12){
+//         faizderecesi = 13;
+//     }else if(12<muddet<24){
+//         faizderecesi = 15;
+//     }else {
+//         faizderecesi = 17;
+//     }
+    
+
+// }
+
+
+// question1
+// let katet1 = +prompt("katet1 i daxil edin");
+// let katet2 = +prompt("katet2 no daxil edin")
+// let hipotenuz = +prompt("hipotenuzu daxil edin");
+
+// if(hipotenuz ** 2 == katet1 ** 2 + katet2 ** 2){
+//    alert("duzbucaqli ucbucaqdir")
+// }else {
+//     alert("duzbucaqli ucbucaq deyil")
+// }
+
+
+// question3
+// let month = +prompt("enter the month");
+// switch (month) {
+//         case "January":
+//         alert("January has 31 days")
+//         break;
+//         case 'February':
+//         alert("February has 28 days")
+//         break;
+//         case 'March':
+//         alert("March has 31 days")
+//         break;
+//         case 'April':
+//         alert("April has 30 days")
+//         break;
+//         case 'May':
+//         alert("May has 31 days")
+//         break;
+//         case 'June':
+//         alert("June has 30 days")
+//         case 'July':
+//         alert("July has 31 days")
+//         break;
+//         case 'August':
+//         alert("August has 31 days")
+//         break;
+//         case 'September':
+//         alert("September has 30 days")
+//         break;
+//         case 'October':
+//         alert("October has 31 days")
+//         break;
+//         case 'November':
+//         alert("November has 30 days")
+//         break;
+//         case 'December':
+//         alert("December has 31 days")
+//         break;
+
+//     default:
+//         alert("bu bir ay deyil");
+//         break;
+// }
+
+
+// clastask3
+
+// let eded1 = +prompt("eded1 i daxil edin");
+// let eded2 = +prompt("eded2 ni daxil edin");
+// let eded3 = +prompt("eded3 u daxil edin");
+
+// if((eded1 + eded2 + eded3 ) / 3){
+//     console.log(("ededi orta") + (eded1 + eded2 +eded3) / 3);
+    
+// }else{
+//     console.log("ededi orta duzgun deyil");
+    
+// }
+
+
+// clastask4
+
+// let name = prompt("pls enter name");
+// let surname = prompt("pls enter surname");
+
+// let fullname = name + surname;
+
+// console.log("adiniz soyadiniz:" + fullname);
+
+
+
