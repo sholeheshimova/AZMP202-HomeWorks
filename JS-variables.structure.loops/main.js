c2c24c0dea6dbf14c0fd20c9a6389a24e5990252
unknown <solhsimova@gmail.com>
2024-12-04 11:07:23 +0400

    variablesloops

--- JS-variables.structure.loops/main.js
+++ JS-variables.structure.loops/main.js
@@ -55,6 +55,48 @@
 
 
 
+//4) for loop'undan istifadə edərək aşağıdakı console'da aşağıdakı görüntünü alın
+//  0 x 0 = 0
+//  1 x 1 = 1
+//  2 x 2 = 4
+//  3 x 3 = 9
+//  4 x 4 = 16
+//  5 x 5 = 25
+//  6 x 6 = 36
+//  7 x 7 = 49
+//  8 x 8 = 64
+//  9 x 9 = 81
+//  10 x 10 = 100
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`${i} x ${i} = ${i * i}`);
+//   }
+
+
+
+
+// 5)  for loop'undan istifadə edərək aşağıdakı console'da aşağıdakı görüntünü alın
+//  i    i^2   i^3
+//  0    0     0
+//  1    1     1
+//  2    4     8
+//  3    9     27
+//  4    16    64
+//  5    25    125
+//  6    36    216
+//  7    49    343
+//  8    64    512
+//  9    81    729
+//  10   100   1000
+
+
+// console.log("i    i^2   i^3"); 
+// for (let i = 0; i <= 10; i++) {
+//   console.log(`${i}    ${i ** 2}    ${i ** 3}`);
+// }
+
+
+
 
 
 // 0 - 100 arası tək ədədlərin və cüt ədədlərin cəmini tapın
