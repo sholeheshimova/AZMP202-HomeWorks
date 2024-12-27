094b8231167028430415643ef2573c6332629358
ClassTime <classtime@code.local>
2024-12-27 14:32:54 +0400

    router-dom

--- react-router-dom/src/pages/Client/Blog/index.jsx
+++ react-router-dom/src/pages/Client/Blog/index.jsx
@@ -6,39 +6,43 @@ import styles from './index.module.scss'
 const Blog = () => {
 
   // const [query, setQuery] = useState('')
-  const [filteredCards, setFilteredCards] = useState([])
-  const [cards, setCards] = useState();
-  const searchInput = useRef(null)
+  const [cards, setCards] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+
   const getCards = async() => {
     const data = await axios("https://fakestoreapi.com/products");
     setCards(data.data)
+    console.log(data.data)
   }
+
   useEffect(() => {
     getCards()
   },[])
 
-  function handleSearch(e){
+  const filtered = cards.filter((blog) => 
+  blog.title.toLowerCase().includes(searchValue.toLowerCase())
+  );
+
+  const handleSearch = (e) => {
     e.preventDefault();
-    
-    const searchValue = searchInput.current.value.toLowerCase()
-    
-    const filtered = cards.filter((blog) => 
-    blog.title.toLowerCase().includes(searchValue)
-    );
-    setFilteredCards(filtered)
+    setSearchValue(e.target.value.trim())
   }
+
+
    return (
     <>
 <form action="">
-  <input type="text"  ref={searchInput} onChange={handleSearch} className={styles.search} placeholder='search..'/>
+  <input type="text" onChange={handleSearch} className={styles.search} placeholder='search..'/>
 </form>
 
     <div className={styles.cardholder} >
-      {filteredCards.map((item) =>(
-        <div key={item.id} className={styles.card} style={{width: 300, height:200}}>
+      {filtered.map((item) =>(
+        <div key={item.id} className={styles.card} style={{width: 300, height:300}}>
+          <img src={item.image} alt="" style={{width: 100, height: 100}}/>
           <h1 style={{fontSize: 20}}>{item.title}</h1>
           <p style={{padding: 20}}>{item.category}</p>
           <p style={{color: "red"}}>Price: {item.price}</p>
+          <button>Details</button>
         </div>
       ))}
     </div>
